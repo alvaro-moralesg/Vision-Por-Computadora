42dddfe08632af158d84cab75ccd4e8cb5f494ba
Alvaro <alv.moralesg@gmail.com>
2021-04-07 22:30:39 -0300

    Practico 2

---
+++ p2/segmentacion.py
@@ -0,0 +1,4 @@
+# Crear un programa que lea una imagen en blanco y negro.
+# Aplicar un umblar sobre los valores de los pixeles
+# Guardar resultado
+# No usar ninguna funcion de OpenCV, excepto leer y guardar
