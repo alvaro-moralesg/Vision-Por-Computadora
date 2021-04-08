af2b0e5acbd93a70a694845ff35af7ca1dccd459
Alvaro <alv.moralesg@gmail.com>
2021-04-07 22:20:26 -0300

    Practico 1

---
+++ p1/guess.py
@@ -0,0 +1,25 @@
+# Crear una funcion 'adivinar' que permita adiviniar
+# un numero generado de forma aleatoria 
+
+import random
+
+def adivinar(intentos):
+	x = random.randint(0,100)
+	print (x)
+	for i in range(1, intentos+1):
+		print ('Intento Nº',i ,': ', end = '')
+		num = int(input())
+		if num == x :
+			print ('Felicidades. Adivino el numero en', i, 'intentos.')
+			exit()
+		else :
+			print ('Numero incorrecto.', end = '')
+	else :
+		print ('\nHa superado el numero de intentos. Suerte para la proxima.')
+		exit()
+
+print ('Adivine el numero generado de forma aleatoria entre 0 y 100.')
+intentos = int (input ('Primero ingrese la cantidad de intentos: '))
+print ('Comencemos')
+adivinar(intentos)
+
